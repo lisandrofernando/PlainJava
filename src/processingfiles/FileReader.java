705b19f9c30b1e57f5ae4cf9d52f3589a3e3d664
lisandrofernando <lisandrusfernandus@gmail.com>
2022-01-20 17:02:57 -0600

    Exception Handling

---
+++ src/processingfiles/FileReader.java
@@ -0,0 +1,37 @@
+package processingfiles;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileReader {
+
+	public FileReader(File file) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		File file = new File("myFile.txt");
+//		
+//		try( FileReader fileReader = new FileReader(file);
+//			BufferedReader bufferedReader = new BufferedReader(fileReader);){
+//			String line = bufferedReader.readLine();
+//		
+//			while(line !=null) {
+//				System.out.println(line);
+//				line = bufferedReader.readLine();
+//			}
+//			
+//		} catch (FileNotFoundException e) {
+//			
+//			System.out.println("File not found");
+//			
+//		}catch (IOException e) {
+//			System.out.println();
+//		}
+	}
+
+}
---
+++ src/processingfiles/Input.java
@@ -0,0 +1,34 @@
+package processingfiles;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Input {
+
+	public static void main(String[] args) throws FileNotFoundException {
+	
+		/*
+		 * The scanner class we will read data or files from the input.
+		 * System.in stands for the input stream
+		 */
+		for(int i=0; i<=3; i++) {
+	    Scanner input = new Scanner(System.in);
+	    System.out.println("Enter some Text: ");
+	 	String enteredText = input.nextLine();
+		System.out.println(enteredText);
+	  }
+		fileInputStream();
+	}
+	
+	public static void fileInputStream() throws FileNotFoundException {
+		
+		File file = new File("myFile.txt");
+		Scanner scan = new Scanner(file);
+		while(scan.hasNext()) {
+			String line = scan.nextLine();
+			System.out.println(line);
+		}
+		scan.close();
+	}
+}
